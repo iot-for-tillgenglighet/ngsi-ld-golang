e5a35a71c0c0583d011a85b5be914b38a6776d72
Isak Styf <isak.styf@masarinconsulting.se>
2020-04-08 22:21:42 +0200

    First commit

---
+++ pkg/datamodels/fiware/weatherobserved.go
@@ -0,0 +1,43 @@
+package fiware
+
+import (
+	ngsi "github.com/iot-for-tillgenglighet/ngsi-ld-golang/pkg/ngsi-ld/types"
+)
+
+//WeatherObserved is an observation of weather conditions at a certain place and time.
+type WeatherObserved struct {
+	ngsi.BaseEntity
+	DateCreated  ngsi.DateTimeProperty    `json:"dateCreated,omitempty"`
+	DateModified ngsi.DateTimeProperty    `json:"dateModified,omitempty"`
+	DateObserved ngsi.DateTimeProperty    `json:"dateObserved"`
+	Location     ngsi.GeoJSONProperty     `json:"location"`
+	RefDevice    *ngsi.DeviceRelationship `json:"refDevice,omitempty"`
+	SnowHeight   *ngsi.NumberProperty     `json:"snowHeight,omitempty"`
+	Temperature  *ngsi.NumberProperty     `json:"temperature,omitempty"`
+}
+
+//NewWeatherObserved creates a new instance of WeatherObserved
+func NewWeatherObserved(device string, latitude float64, longitude float64, observedAt string) *WeatherObserved {
+	dateTimeValue := ngsi.CreateDateTimeProperty(observedAt)
+	refDevice := ngsi.CreateDeviceRelationshipFromDevice(device)
+
+	if refDevice == nil {
+		device = "manual"
+	}
+
+	id := "urn:ngsi-ld:WeatherObserved:" + device + ":" + observedAt
+
+	return &WeatherObserved{
+		DateObserved: *dateTimeValue,
+		Location:     ngsi.CreateGeoJSONPropertyFromWGS84(latitude, longitude),
+		RefDevice:    refDevice,
+		BaseEntity: ngsi.BaseEntity{
+			ID:   id,
+			Type: "WeatherObserved",
+			Context: []string{
+				"https://schema.lab.fiware.org/ld/context",
+				"https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld",
+			},
+		},
+	}
+}
---
+++ pkg/ngsi-ld/types/types.go
@@ -0,0 +1,99 @@
+package types
+
+//BaseEntity contains the required base properties an Entity must have
+type BaseEntity struct {
+	ID      string   `json:"id"`
+	Type    string   `json:"type"`
+	Context []string `json:"@context"`
+}
+
+//Property contains the mandatory Type property
+type Property struct {
+	Type string `json:"type"`
+}
+
+//DateTimeProperty stores date and time values (surprise, surprise ...)
+type DateTimeProperty struct {
+	Property
+	Value struct {
+		Type  string `json:"@type"`
+		Value string `json:"@value"`
+	} `json:"value"`
+}
+
+//CreateDateTimeProperty creates a property from a UTC time stamp
+func CreateDateTimeProperty(value string) *DateTimeProperty {
+	dtp := &DateTimeProperty{
+		Property: Property{Type: "Property"},
+	}
+
+	dtp.Value.Type = "DateTime"
+	dtp.Value.Value = value
+
+	return dtp
+}
+
+//GeoJSONProperty is used to store lat/lon coordinates
+type GeoJSONProperty struct {
+	Property
+	Value struct {
+		Type        string     `json:"type"`
+		Coordinates [2]float64 `json:"coordinates"`
+	} `json:"value"`
+}
+
+//CreateGeoJSONPropertyFromWGS84 creates a GeoJSONProperty from a WGS84 coordinate
+func CreateGeoJSONPropertyFromWGS84(latitude, longitude float64) GeoJSONProperty {
+	p := GeoJSONProperty{
+		Property: Property{Type: "GeoProperty"},
+	}
+
+	p.Value.Type = "Point"
+	p.Value.Coordinates[0] = longitude
+	p.Value.Coordinates[1] = latitude
+
+	return p
+}
+
+//NumberProperty holds a float64 Value
+type NumberProperty struct {
+	Property
+	Value float64 `json:"value"`
+}
+
+//NewNumberProperty is a convenience function for creating NumberProperty instances
+func NewNumberProperty(value float64) *NumberProperty {
+	return &NumberProperty{
+		Property: Property{Type: "Property"},
+		Value:    value,
+	}
+}
+
+//TextProperty stores values of type text
+type TextProperty struct {
+	Property
+	Value string `json:"value"`
+}
+
+//Relationship is a base type for all types of relationships
+type Relationship struct {
+	Type string `json:"type"`
+}
+
+//DeviceRelationship stores information about an entity's relation to a certain Device
+type DeviceRelationship struct {
+	Relationship
+	Object string `json:"object"`
+}
+
+//CreateDeviceRelationshipFromDevice create a DeviceRelationship from a Device
+func CreateDeviceRelationshipFromDevice(device string) *DeviceRelationship {
+	if len(device) == 0 {
+		return nil
+	}
+
+	return &DeviceRelationship{
+		Relationship: Relationship{Type: "Relationship"},
+		Object:       "urn:ngsi-ld:Device:" + device,
+	}
+}
