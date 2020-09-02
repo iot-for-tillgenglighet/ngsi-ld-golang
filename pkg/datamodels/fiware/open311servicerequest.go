c93162ee62438e362e965d11c94e8914d63f5f16
Sofia Styf <hsestyf@hotmail.co.uk>
2020-09-02 15:49:19 +0200

    linting

--- pkg/datamodels/fiware/open311servicerequest.go
+++ pkg/datamodels/fiware/open311servicerequest.go
@@ -16,12 +16,12 @@ type Open311ServiceRequest struct {
 func NewOpen311ServiceRequest(latitude float64, longitude float64, reportedType int, reportedTimestamp string) *Open311ServiceRequest {
 	dateTimeValue := ngsi.CreateDateTimeProperty(reportedTimestamp)
 
-	id := "urn:ngsi-ld:Open311ServiceRequest:" + reportedTimestamp 
+	id := "urn:ngsi-ld:Open311ServiceRequest:" + reportedTimestamp
 
 	return &Open311ServiceRequest{
-		requested_datetime : dateTimeValue,
-		Location:     ngsi.CreateGeoJSONPropertyFromWGS84(latitude, longitude),
-		service_code: *ngsi.NewNumberPropertyFromInt(reportedType)
+		requested_datetime: dateTimeValue,
+		Location:           ngsi.CreateGeoJSONPropertyFromWGS84(latitude, longitude),
+		service_code:       *ngsi.NewNumberPropertyFromInt(reportedType),
 		BaseEntity: ngsi.BaseEntity{
 			ID:   id,
 			Type: "Open311ServiceRequest",
