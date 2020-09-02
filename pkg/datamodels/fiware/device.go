617a05ad1a677c534eef51ccb565f308777d99a3
Sofia Styf <hsestyf@hotmail.co.uk>
2020-09-02 15:48:44 +0200

    added device entity

---
+++ pkg/datamodels/fiware/device.go
@@ -0,0 +1,29 @@
+package fiware
+
+import (
+	ngsi "github.com/iot-for-tillgenglighet/ngsi-ld-golang/pkg/ngsi-ld/types"
+)
+
+type Device struct {
+	ngsi.BaseEntity
+	Value        *ngsi.TextProperty     `json:"value"`
+	DateCreated  *ngsi.DateTimeProperty `json:"dateCreated,omitempty"`
+	DateModified *ngsi.DateTimeProperty `json:"dateModified,omitempty"`
+}
+
+//NewDevice creates a new Device from given ID and Value
+func NewDevice(id string, value string) *Device {
+	id = "urn:ngsi-ld:Device:" + id
+
+	return &Device{
+		Value: ngsi.NewTextProperty(value),
+		BaseEntity: ngsi.BaseEntity{
+			ID:   id,
+			Type: "Device",
+			Context: []string{
+				"https://schema.lab.fiware.org/ld/context",
+				"https://uri.etsi.org/ngsi-ld/v1/ngsi-ld-core-context.jsonld",
+			},
+		},
+	}
+}
